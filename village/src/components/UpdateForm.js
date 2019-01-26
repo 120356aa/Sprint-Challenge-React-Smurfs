4b89e99450548e379c935f1d29161b81d73bfe62
120356aa <yurlovandrew@gmail.com>
2019-01-26 09:28:13 -0800

    created updateform js

---
+++ village/src/components/UpdateForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+class UpdateForm extends Component {
+
+	render() {
+		return(
+
+		);
+	}
+}
+
+export default UpdateForm;
