b2978eb9602986ac5189bde64851a4e170aba030
120356aa <yurlovandrew@gmail.com>
2019-01-26 10:03:49 -0800

    created form

--- village/src/components/UpdateForm.js
+++ village/src/components/UpdateForm.js
@@ -31,9 +31,35 @@ class UpdateForm extends Component {
 			.catch(err => console.log(err));
 	}
 
+	handleInputChange = e => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
 	render() {
 		return(
-			<div></div>
+			<div>
+				<form onSubmit={this.updateSmurf}> 
+					<input
+						onChange={this.handleInputChange}
+						placeholder="name"
+						value={this.state.name}
+						name="name"
+					/>
+					<input
+						onChange={this.handleInputChange}
+						placeholder="age"
+						value={this.state.age}
+						name="age"
+					/>
+					<input 
+						onChange={this.handleInputChange}
+						placeholder="height"
+						value={this.state.height}
+						name="height"
+					/>
+					<button type="submit">Add Smurf</button>
+				</form>
+			</div>
 		);
 	}
 }
